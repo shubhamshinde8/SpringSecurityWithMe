f236ab38b390e7785ee28426aa3947665c3c89b7
Shubham Shinde <shubhamshinde8746@gmail.com>
2025-05-01 00:15:45 -1200

    understanding csrf token and how it works

--- src/main/java/com/shubham/controller/WelcomeController.java
+++ src/main/java/com/shubham/controller/WelcomeController.java
@@ -1,16 +1,39 @@
 package com.shubham.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
+    private record Student(
+            String name,
+            String address
+    ){}
+
+    List<Student>  students = new ArrayList<>(
+            List.of(
+                    new Student("Shubham","Sillod"),
+                    new Student("Vishal", "Shinde")
+            )
+    );
 
     @GetMapping("/")
-    public String getMsg(){
-        
-        return "Hello welcome to spring security ";
+    public List<Student> getStudents(){
+        return students;
+    }
+
+    @PostMapping("/")
+    public Student student(@RequestBody Student student){
+        students.add(student);
+        return student;
     }
     
 }
