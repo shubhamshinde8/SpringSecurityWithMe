f3c6e9b8c60237c7a53d0466816f2d325d28afa4
Shubham Shinde <shubhamshinde8746@gmail.com>
2025-04-30 17:04:15 -1200

    initial commit

---
+++ src/main/java/com/shubham/controller/WelcomeController.java
@@ -0,0 +1,16 @@
+package com.shubham.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+
+    @GetMapping("/")
+    public String getMsg(){
+        
+        return "Hello welcome to spring security ";
+    }
+    
+}
